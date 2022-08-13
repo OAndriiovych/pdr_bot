ba86b94ccc96324c6ff61ffa08c540471f5a3c49
narnanam <narnanam@gmail.com>
2022-08-13 18:21:21 +0300

    init

---
+++ src/main/java/org/pdr/Main.java
@@ -0,0 +1,41 @@
+package org.pdr;
+
+import org.pdr.utils.MyProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main extends TelegramLongPollingBot {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Bot started");
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Main());
+        } catch (TelegramApiException e) {
+            logger.error("ERROR in Main method", e);
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return MyProperties.getTelegramBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return MyProperties.getTelegramBotToken();
+    }
+}
---
+++ src/main/java/org/pdr/utils/MyProperties.java
@@ -0,0 +1,31 @@
+package org.pdr.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MyProperties {
+    private static final String PATH = "src/main/resources/bot.properties";
+    private static final Properties PROPERTIES = getProperty();
+
+    private MyProperties() {
+    }
+
+    public static String getTelegramBotToken() {
+        return PROPERTIES.getProperty("bot.token");
+    }
+
+    public static String getTelegramBotName() {
+        return PROPERTIES.getProperty("bot.name");
+    }
+
+    private static Properties getProperty() {
+        Properties properties = new Properties();
+        try (FileInputStream fis = new FileInputStream(PATH)) {
+            properties.load(fis);
+        } catch (IOException e) {
+            //#TODO
+        }
+        return properties;
+    }
+}
